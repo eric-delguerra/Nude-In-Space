cc7c9f409e923e230b56a97dc3f9e5b81a6d4726
eric-delguerra <delguerra.eric@gmail.com>
2020-05-01 21:18:58 +0200

    Init project
    Add player movement and area limits

---
+++ Assets/LimitArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitArea : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        WarningEngine.instance.EnterArea();
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        WarningEngine.instance.ExitArea();
+    }
+}
