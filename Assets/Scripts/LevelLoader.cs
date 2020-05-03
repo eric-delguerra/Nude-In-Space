53018859bc29d14367d7b09e56fdf54727b09da5
eric-delguerra <delguerra.eric@gmail.com>
2020-05-03 12:31:40 +0200

    Add transitions

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime;
+
+    public void loadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    public IEnumerator LoadLevel(int levelIndex)
+    {
+        // Play Animation
+        transition.SetTrigger("Start");
+
+        // wait
+        yield return new WaitForSeconds(transitionTime);
+
+        // Load scene
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
